d264de3b5180be874df53846110a9c924c23eade
David Faltus <dawop123@seznam.cz>
2025-04-07 12:12:55 +0200

    CVICENI 9.

--- src/main/java/pro1/Main3.java
+++ src/main/java/pro1/Main3.java
@@ -1,6 +1,10 @@
 package pro1;
 
+import com.google.gson.Gson;
 import pro1.apiDataModel.ActionsList;
+import pro1.apiDataModel.TeachersList;
+
+import java.util.Comparator;
 
 public class Main3 {
 
@@ -11,15 +15,21 @@ public class Main3 {
     public static String emailOfBestTeacher(String department, int year)
     {
         // TODO 3.2:
-        //  - Stáhni seznam učitelů na katedře
-        //  - Stáhni seznam akcí na katedře
-        //  - Najdi učitele s nejvyšším "score" a vrať jeho e-mail
+        String json1 = Api.getActionsByDepartment(department,year);
+        ActionsList actions = new Gson().fromJson(json1, ActionsList.class);
 
-        return "";
+        String json2 = Api.getTeachersByDepartment(department);
+        TeachersList teacher = new Gson().fromJson(json2, TeachersList.class);
+
+        return teacher.items.stream().max(Comparator.comparing(t ->
+                TeacherScore(t.id, actions))).get().email;
     }
 
     public static long TeacherScore(long teacherId, ActionsList departmentSchedule)
     {
-        return 0; // TODO 3.1: Doplň pomocnou metodu - součet všech přihlášených studentů na akcích daného učitele
+        return  departmentSchedule.items.stream().filter(action ->
+                action.teacherId == teacherId).mapToInt(action ->
+                action.personsCount).sum();
+        // TODO 3.1: Doplň pomocnou metodu - součet všech přihlášených studentů na akcích daného učitele
     }
 }
