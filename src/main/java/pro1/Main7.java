d7ca532c5b247b13eb4e72d00576c7f557afb6f1
David Faltus <st67025@upce.cz>
2025-04-27 21:15:51 +0200

    MAIN 7 KOMPLETACE

--- src/main/java/pro1/Main7.java
+++ src/main/java/pro1/Main7.java
@@ -1,7 +1,27 @@
 package pro1;
 
+import com.google.gson.Gson;
+import pro1.apiDataModel.ActionsList;
+import pro1.apiDataModel.SpecializationsList;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
 public class Main7 {
-    public static String specializationDeadlines(int i) {
-        return null;
+    public static String specializationDeadlines(int year)
+    {
+        String json = Api.getSpecializations(year);
+        SpecializationsList specialization = new Gson().fromJson(json, SpecializationsList.class);
+
+        Comparator<String> comparator = Comparator
+                .comparing((String s) -> Integer.parseInt(s.split("\\.")[2]))
+                .thenComparing(s -> Integer.parseInt(s.split("\\.")[1]))
+                .thenComparing(s -> Integer.parseInt(s.split("\\.")[0]));
+
+        return specialization.specializations.stream()
+                .map(s -> s.eprDeadlinePrihlaska.value)
+                .distinct()
+                .sorted(comparator)
+                .collect(Collectors.joining(","));
     }
 }
